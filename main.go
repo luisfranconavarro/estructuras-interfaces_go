b35ba4ae8c118ea04d1d93a85b037e5f5a81388c
Luis Franco <jose.fnavarro@alumnos.udg.mx>
2020-10-27 20:06:38 -0600

    correcion con el uso de bufio para leer strings

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import(
 	"fmt"
+	"bufio"
+	"os"
 	"./archivos_mul"
 )
 
@@ -10,52 +12,54 @@ func main()  {
 	var dato3 uint
 	op := 0
 
+	scanner := bufio.NewScanner(os.Stdin)
+
 	contenido := archivos_mul.ContenidoWeb{
 		Mul: []archivos_mul.Multimedia{},
 	}
-	
-	for i := 0; i < 1; i++ {
+
+	for true {
 		fmt.Println("\nMenu\n1.- Capturar imagen\n2.- Capturar audio\n3.- Capturar video\n4.- Mostrar elementos guardados\n0.- Salir")
-		fmt.Scan(&op)
+		fmt.Scanln(&op)
 		if op == 1{
 			fmt.Println("\nImagen")
 			fmt.Print("Titulo: ")
-			fmt.Scan(&dato1)
+			scanner.Scan()
+			dato1 = scanner.Text()
 			fmt.Print("Formato: ")
-			fmt.Scan(&dato2)
+			fmt.Scanln(&dato2)
 			fmt.Print("Canales: ")
-			fmt.Scan(&dato3)
+			fmt.Scanln(&dato3)
 			archivo := archivos_mul.Imagen{Titulo: dato1, Formato: dato2, Canales: dato3}
 			contenido.Mul = append(contenido.Mul,&archivo)
-			i--
-		}
-		if op == 2{
+		}else if op == 2{
 			fmt.Println("\nAudio")
 			fmt.Print("Titulo: ")
-			fmt.Scan(&dato1)
+			scanner.Scan()
+			dato1 = scanner.Text()
 			fmt.Print("Formato: ")
-			fmt.Scan(&dato2)
+			fmt.Scanln(&dato2)
 			fmt.Print("Duracion en segundos: ")
-			fmt.Scan(&dato3)
+			fmt.Scanln(&dato3)
 			archivo := archivos_mul.Audio{Titulo: dato1, Formato: dato2, Duracion: dato3}
 			contenido.Mul = append(contenido.Mul,&archivo)
-			i--
-		}
-		if op == 3{
+		}else if op == 3{
 			fmt.Println("\nVideo")
 			fmt.Print("Titulo: ")
-			fmt.Scan(&dato1)
+			scanner.Scan()
+			dato1 = scanner.Text()
 			fmt.Print("Formato: ")
-			fmt.Scan(&dato2)
+			fmt.Scanln(&dato2)
 			fmt.Print("Frames: ")
-			fmt.Scan(&dato3)
+			fmt.Scanln(&dato3)
 			archivo := archivos_mul.Video{Titulo: dato1, Formato: dato2, Frames: dato3}
 			contenido.Mul = append(contenido.Mul,&archivo)
-			i--
-		}
-		if op == 4{
+		}else if op == 4{
 			contenido.Mostrar()
-			i--
+		}else if op == 0{
+			break
+		}else{
+			fmt.Println("Opcion incorrecta")
 		}
 	}
 	fmt.Println("\nHasta pronto\n")
